e87c64debe1abad73b9e689274c17490b8289e49
abdullahNavin <navinfayed26@gmail.com>
2026-02-01 20:40:32 +0600

    install better auth

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import app from "./app"
+import { prisma } from "./lib/prisma"
 
 const port = process.env.PORT || 5000
 
-const main = () => {
-    app.listen(port, () => {
-        console.log(`skillBridge server is running on port ${port}`);
-    })
+const main = async () => {
+    try {
+        await prisma.$connect()
+        console.log('successfully connect with database');
+
+        app.listen(port, () => {
+            console.log(`skillBridge server is running on port ${port}`);
+        })
+
+    } catch (error) {
+        console.log(error);
+        await prisma.$disconnect()
+        process.exit(1)
+    }
 }
 
 main()
